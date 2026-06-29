09db081368e4bf4346a2bc4430fa93d7c894aaf7
Kamil-Shehzad <75947637+Kamil-shehzad@users.noreply.github.com>
2026-06-29 10:47:02 +0500

    making tables and starting making api routes (make register route))

---
+++ app/Http/Controllers/Account.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class Account extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validate the request data
+        $validate = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json(['status' => 400, 'errors' => $validate->errors()],400);
+        }
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json(['status' => 200, 'message' => 'User registered successfully', 'user' => $user],200);
+    }
+}
